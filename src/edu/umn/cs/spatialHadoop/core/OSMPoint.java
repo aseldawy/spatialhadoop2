7f11654b9fe62cd4b0b58faff4889337c2647563
Ahmed Eldawy <eldawy@cs.umn.edu>
2013-07-03 20:44:14 -0500

    Move Serialize/Deserialize of Tag map to TextSerializerHelper for modularity and code reuse

--- src/edu/umn/cs/spatialHadoop/core/OSMPoint.java
+++ src/edu/umn/cs/spatialHadoop/core/OSMPoint.java
@@ -14,64 +14,24 @@ import edu.umn.cs.spatialHadoop.io.TextSerializerHelper;
 public class OSMPoint extends Point {
   public long id;
   public Map<String, String> tags = new HashMap<String, String>();
-  private static final byte[] Separators = {'[', '#', ',', ']'};
 
   @Override
   public void fromText(Text text) {
     id = TextSerializerHelper.consumeLong(text, '\t');
     x = TextSerializerHelper.consumeDouble(text, '\t');
     y = TextSerializerHelper.consumeDouble(text, '\t');
-    tags.clear();
-    if (text.getLength() > 0) {
-      byte[] tagsBytes = text.getBytes();
-      if (tagsBytes[0] != '[')
-        return;
-      int i1 = 1;
-      while (i1 < text.getLength() && tagsBytes[i1] != ']') {
-        int i2 = i1 + 1;
-        while (i2 < text.getLength() && tagsBytes[i2] != '#')
-          i2++;
-        String key = new String(tagsBytes, i1, i2 - i1);
-        i1 = i2 + 1;
-        
-        i2 = i1 + 1;
-        while (i2 < text.getLength() && tagsBytes[i2] != ',' && tagsBytes[i2] != ']')
-          i2++;
-        String value = new String(tagsBytes, i1, i2 - i1);
-        tags.put(key, value);
-        i1 = i2;
-        if (i1 < text.getLength() && tagsBytes[i1] == ',')
-          i1++;
-      }
-    }
+    TextSerializerHelper.consumeMap(text, tags);
   }
-  
+
   @Override
   public Text toText(Text text) {
     TextSerializerHelper.serializeLong(id, text, '\t');
     TextSerializerHelper.serializeDouble(x, text, '\t');
     TextSerializerHelper.serializeDouble(y, text, tags.isEmpty() ? '\0' : '\t');
-    if (!tags.isEmpty()) {
-      boolean first = true;
-      text.append(Separators, 0, 1);
-      for (Map.Entry<String, String> entry : tags.entrySet()) {
-        if (first) {
-          first = false;
-        } else {
-          first = true;
-          text.append(Separators, 2, 1);
-        }
-        byte[] k = entry.getKey().getBytes();
-        text.append(k, 0, k.length);
-        text.append(Separators, 1, 1);
-        byte[] v = entry.getValue().getBytes();
-        text.append(v, 0, v.length);
-      }
-      text.append(Separators, 3, 1);
-    }
+    TextSerializerHelper.serializeMap(text, tags);
     return text;
   }
-  
+
   @Override
   public void write(DataOutput out) throws IOException {
     out.writeLong(id);
