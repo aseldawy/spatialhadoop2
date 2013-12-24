0abb08f57c977f34362bbf7f780795d267f39186
Ahmed Eldawy <eldawy@cs.umn.edu>
2013-12-24 16:28:56 +0300

    Handle different zoom levels correctly with Mercator projection

--- src/edu/umn/cs/spatialHadoop/core/NASAPoint.java
+++ src/edu/umn/cs/spatialHadoop/core/NASAPoint.java
@@ -70,7 +70,7 @@ public class NASAPoint extends Point {
     double latRad = this.y * Math.PI / 180.0;
     double mercN = Math.log(Math.tan((Math.PI/4)+(latRad/2)));
     //int imageY = (int) (((vflip? -this.y : this.y) - fileMBR.y1) * imageHeight / fileMBR.getHeight());
-    int imageY = (int) (((double) imageHeight / 2) - (imageWidth * mercN / (2 * Math.PI)));
+    int imageY = (int) (((double) imageHeight / 2) - (imageWidth * mercN / (2 * (fileMBR.getHeight() * Math.PI / 180.0))));
     
     if (value > 0 && imageX >= 0 && imageX < imageWidth && imageY >= 0 && imageY < imageHeight) {
       Color color;
