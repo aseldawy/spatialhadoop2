16a3f379e1ec0013732d698537922396fbe38d6c
Ahmed Eldawy <eldawy@cs.umn.edu>
2014-09-18 23:05:16 -0500

    Fixed an error with the main function of AggregateQuadTree

--- src/edu/umn/cs/spatialHadoop/nasa/AggregateQuadTree.java
+++ src/edu/umn/cs/spatialHadoop/nasa/AggregateQuadTree.java
@@ -42,6 +42,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Writable;
@@ -796,7 +797,6 @@ public class AggregateQuadTree {
           "LST_Day_1km",
           indexedFilePath);
     }
-    
     
     System.exit(0);
     
