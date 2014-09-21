b87c687d91c53494950f03a44fa5bf4a23bf67ad
Ibrahim Sabek <ibrahimsabek@Ibrahims-MacBook-Pro.local>
2014-09-21 00:17:13 -0500

    Distributed version of Spatio-Temporal indexing using AggregateQuadTree

---
+++ src/edu/umn/cs/spatialHadoop/nasa/DistributedSpatioTemporalIndexer.java
@@ -0,0 +1,224 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the
+ * NOTICE file distributed with this work for additional information regarding copyright ownership. The ASF
+ * licenses this file to you under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package edu.umn.cs.spatialHadoop.nasa;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.ClusterStatus;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import edu.umn.cs.spatialHadoop.OperationsParams;
+import edu.umn.cs.spatialHadoop.mapred.TextOutputFormat;
+import edu.umn.cs.spatialHadoop.util.FileUtil;
+import edu.umn.cs.spatialHadoop.util.NASADatasetUtil;
+import edu.umn.cs.spatialHadoop.util.TemporalIndexManager;
+
+/**
+ * Distributed version of Spatio-Temporal Indexer based on AggregateQuadTree.
+ * Hadoop Map-Reduce framework is used to implement the distribution algorithm.
+ * 
+ * @author ibrahimsabek
+ *
+ */
+public class DistributedSpatioTemporalIndexer {
+
+	/** Logger */
+	private static final Log LOG = LogFactory
+			.getLog(DistributedSpatioTemporalIndexer.class);
+
+	private static Path hdfsIndexPath = null;
+
+	public static class AggregateQuadTreeMaper extends MapReduceBase implements
+			Mapper<LongWritable, Text, Text, Text> {
+
+		private Text success = new Text("true");
+		private Text failure = new Text("false");
+
+		@Override
+		public void map(LongWritable dummy, Text hdfFilePathText,
+				OutputCollector<Text, Text> output, Reporter reporter)
+				throws IOException {
+
+			if (hdfsIndexPath == null) {
+				LOG.warn("Index path for " + hdfFilePathText.toString()
+						+ " is not setted");
+				output.collect(hdfFilePathText, failure);
+				return;
+			}
+
+			// create a template AggregateQuadTree node
+			AggregateQuadTree.getOrCreateStockQuadTree(1200);
+
+			String hdfFilePathString = hdfFilePathText.toString();
+			Path hdfFilePath = new Path(hdfFilePathString);
+			String hdfIndexFileName = hdfFilePathString.substring(
+					hdfFilePathString.lastIndexOf("/") + 1,
+					hdfFilePathString.length());
+			Path hdfIndexFilePath = new Path(hdfsIndexPath.toString() + "/"
+					+ hdfIndexFileName);
+			try {
+				AggregateQuadTree.build(new Configuration(), hdfFilePath,
+						"LST_Day_1km", hdfIndexFilePath);
+				output.collect(hdfFilePathText, success);
+			} catch (Exception e) {
+
+				LOG.warn("Failed to build AggregateQuadTree for "
+						+ hdfFilePathText.toString() + ": " + e.getMessage());
+				output.collect(hdfFilePathText, failure);
+			}
+
+		}
+	}
+
+	/**
+	 * Build a bunch of AggregateQuadTrees using a Map-Reduce job
+	 * 
+	 * @param inputPathsDictionaryPath
+	 * @param params
+	 * @return
+	 * @throws IOException
+	 */
+	public static void aggregateQuadTreeMapReduce(
+			Path inputPathsDictionaryPath, OperationsParams params)
+			throws IOException {
+
+		// configure a map-reduce job
+		JobConf job = new JobConf(params,
+				DistributedSpatioTemporalIndexer.class);
+
+		Path outputPath;
+		String outputPathPrefix = "aggQuadTree_";
+		FileSystem outFs = FileSystem.get(job);
+		do {
+			outputPath = new Path(outputPathPrefix
+					+ (int) (Math.random() * 1000000));
+		} while (outFs.exists(outputPath));
+
+		job.setJobName("AggregateQuadTree");
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setMapperClass(AggregateQuadTreeMaper.class);
+
+		ClusterStatus clusterStatus = new JobClient(job).getClusterStatus();
+		job.setNumMapTasks(clusterStatus.getMaxMapTasks() * 5);
+		job.setInputFormat(TextInputFormat.class);
+		job.setOutputFormat(TextOutputFormat.class);
+
+		TextInputFormat.setInputPaths(job, inputPathsDictionaryPath);
+		TextOutputFormat.setOutputPath(job, outputPath);
+
+		// Submit the job
+		JobClient.runJob(job);
+	}
+
+	/**
+	 * Set the HDFS path of the desired index
+	 * 
+	 * @param hdfsIndexPath
+	 */
+	public static void setIndexPath(Path hdfsIndexPath) {
+		DistributedSpatioTemporalIndexer.hdfsIndexPath = hdfsIndexPath;
+	}
+
+	public static void main(String[] args) throws IOException, ParseException {
+
+		OperationsParams params = new OperationsParams(
+				new GenericOptionsParser(args));
+		String timeRange = params.get("time"); //time range
+		Path datasetPath = params.getPaths()[0]; //dataset path
+		Path indexesPath = params.getPaths()[1]; //index path
+
+		TemporalIndexManager temporalIndexManager = new TemporalIndexManager(
+				datasetPath, indexesPath);
+		temporalIndexManager.prepareNeededIndexes(timeRange);
+
+		// Indexes need to be built or re-built using AggregateQuadTreeMapReduce
+		Path[] dailyIndexes = temporalIndexManager.getNeededDailyIndexes();
+		for (Path dailyIndexPath : dailyIndexes) {	
+			FileSystem currFileSystem = dailyIndexPath.getFileSystem(new Configuration());
+			Path[] dailyIndexHDFFiles = NASADatasetUtil.getFilesListInPath(dailyIndexPath);
+			Path dailyIndexDictionaryPath = FileUtil.writePathsToFile(params,
+					dailyIndexHDFFiles);
+			Path dailyIndexOutputPath = new Path(temporalIndexManager
+					.getDailyIndexesHomePath().toString() + "/"
+					+ NASADatasetUtil.extractDateStringFromPath(dailyIndexPath));
+			
+			if(currFileSystem.exists(dailyIndexOutputPath)){
+				currFileSystem.delete(dailyIndexOutputPath, true);
+			}
+			currFileSystem.mkdirs(dailyIndexOutputPath);
+			
+			DistributedSpatioTemporalIndexer.setIndexPath(dailyIndexOutputPath);
+			aggregateQuadTreeMapReduce(dailyIndexDictionaryPath, params);
+
+			currFileSystem.delete(dailyIndexDictionaryPath, false);
+		}
+
+		// Indexes need to be merged or re-merged
+		Path[] monthlyIndexes = temporalIndexManager.getNeededMontlyIndexes();
+		for (Path monthlyIndexPath : monthlyIndexes) {
+			FileSystem currFileSystem = monthlyIndexPath.getFileSystem(new Configuration());
+			ArrayList<Path[]> pathsArrList = NASADatasetUtil.getSortedTuplesInPath(temporalIndexManager.getDailyIndexesHomePath(),
+					NASADatasetUtil.extractDateStringFromPath(monthlyIndexPath));
+			
+			if(currFileSystem.exists(monthlyIndexPath)){
+				currFileSystem.delete(monthlyIndexPath, true);
+			}
+			currFileSystem.mkdirs(monthlyIndexPath);
+			
+			for(Path[] currDailyIndexHDFFiles: pathsArrList){
+				Path currMonthlyIndexHDFFilePath = new Path(monthlyIndexPath.toString() + 
+						"/" + NASADatasetUtil.getHDFfilePattern(currDailyIndexHDFFiles[0].toString()) +".hdf");
+				AggregateQuadTree.merge(new Configuration(), currDailyIndexHDFFiles, currMonthlyIndexHDFFilePath);
+			}			
+		}
+
+		// Indexes need to be merged or re-merged
+		Path[] yearlyIndexes = temporalIndexManager.getNeededYearlyIndexes();
+		for (Path yearlyIndexPath : yearlyIndexes) {
+			FileSystem currFileSystem = yearlyIndexPath.getFileSystem(new Configuration());
+			ArrayList<Path[]> pathsArrList = NASADatasetUtil.getSortedTuplesInPath(temporalIndexManager.getMonthlyIndexesHomePath(),
+					NASADatasetUtil.extractDateStringFromPath(yearlyIndexPath));
+			
+			if(currFileSystem.exists(yearlyIndexPath)){
+				currFileSystem.delete(yearlyIndexPath, true);
+			}
+			currFileSystem.mkdirs(yearlyIndexPath);
+			
+			for(Path[] currMonthlyIndexHDFFiles: pathsArrList){
+				Path currYearlyIndexHDFFilePath = new Path(yearlyIndexPath.toString() + 
+						"/" + NASADatasetUtil.getHDFfilePattern(currMonthlyIndexHDFFiles[0].toString()) +".hdf");
+				AggregateQuadTree.merge(new Configuration(), currMonthlyIndexHDFFiles, currYearlyIndexHDFFilePath);
+			}			
+		}
+
+	}
+
+}
--- src/edu/umn/cs/spatialHadoop/util/FileUtil.java
+++ src/edu/umn/cs/spatialHadoop/util/FileUtil.java
@@ -12,81 +12,157 @@
  */
 package edu.umn.cs.spatialHadoop.util;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapred.FileSplit;
 
+import edu.umn.cs.spatialHadoop.OperationsParams;
+
 /**
  * A bunch of helper functions used with files
+ * 
  * @author Ahmed Eldawy
- *
  */
 public final class FileUtil {
 
-  public static String copyFile(Configuration job, FileStatus fileStatus) throws IOException {
-    return FileUtil.copyFileSplit(job, new FileSplit(fileStatus.getPath(), 0, fileStatus.getLen(), new String[0]));
-  }
+	public static String copyFile(Configuration job, FileStatus fileStatus)
+			throws IOException {
+		return FileUtil.copyFileSplit(job, new FileSplit(fileStatus.getPath(),
+				0, fileStatus.getLen(), new String[0]));
+	}
 
-  /**
-   * Copies a part of a file from a remote file system (e.g., HDFS) to a local
-   * file. Returns a path to a local temporary file.
-   * @param conf
-   * @param split
-   * @return
-   * @throws IOException 
-   */
-  public static String copyFileSplit(Configuration conf, FileSplit split) throws IOException {
-    FileSystem fs = split.getPath().getFileSystem(conf);
-  
-    // Special case of a local file. Skip copying the file
-    if (fs instanceof LocalFileSystem && split.getStart() == 0)
-      return split.getPath().toUri().getPath();
-  
-    // Length of input file. We do not depend on split.length because it is not
-    // set by input format for performance reason. Setting it in the input
-    // format would cost a lot of time because it runs on the client machine
-    // while the record reader runs on slave nodes in parallel
-    long length = fs.getFileStatus(split.getPath()).getLen();
-  
-    FSDataInputStream in = fs.open(split.getPath());
-    in.seek(split.getStart());
-    
-    // Prepare output file for write
-    File tempFile = File.createTempFile(split.getPath().getName(), "hdf");
-    OutputStream out = new FileOutputStream(tempFile);
-    
-    // A buffer used between source and destination
-    byte[] buffer = new byte[1024*1024];
-    while (length > 0) {
-      int numBytesRead = in.read(buffer, 0, (int)Math.min(length, buffer.length));
-      out.write(buffer, 0, numBytesRead);
-      length -= numBytesRead;
-    }
-    
-    in.close();
-    out.close();
-    return tempFile.getAbsolutePath();
-  }
+	/**
+	 * Copies a part of a file from a remote file system (e.g., HDFS) to a local
+	 * file. Returns a path to a local temporary file.
+	 * 
+	 * @param conf
+	 * @param split
+	 * @return
+	 * @throws IOException
+	 */
+	public static String copyFileSplit(Configuration conf, FileSplit split)
+			throws IOException {
+		FileSystem fs = split.getPath().getFileSystem(conf);
 
-  /**
-   * Copies a file to the local file system given its path.
-   * @param conf
-   * @param inFile
-   * @return
-   * @throws IOException
-   */
-  public static String copyFile(Configuration conf, Path inFile) throws IOException {
-    FileSystem fs = inFile.getFileSystem(conf);
-    return copyFile(conf, fs.getFileStatus(inFile));
-  }
+		// Special case of a local file. Skip copying the file
+		if (fs instanceof LocalFileSystem && split.getStart() == 0)
+			return split.getPath().toUri().getPath();
 
+		// Length of input file. We do not depend on split.length because it is
+		// not
+		// set by input format for performance reason. Setting it in the input
+		// format would cost a lot of time because it runs on the client machine
+		// while the record reader runs on slave nodes in parallel
+		long length = fs.getFileStatus(split.getPath()).getLen();
+
+		FSDataInputStream in = fs.open(split.getPath());
+		in.seek(split.getStart());
+
+		// Prepare output file for write
+		File tempFile = File.createTempFile(split.getPath().getName(), "hdf");
+		OutputStream out = new FileOutputStream(tempFile);
+
+		// A buffer used between source and destination
+		byte[] buffer = new byte[1024 * 1024];
+		while (length > 0) {
+			int numBytesRead = in.read(buffer, 0,
+					(int) Math.min(length, buffer.length));
+			out.write(buffer, 0, numBytesRead);
+			length -= numBytesRead;
+		}
+
+		in.close();
+		out.close();
+		return tempFile.getAbsolutePath();
+	}
+
+	/**
+	 * Copies a file to the local file system given its path.
+	 * 
+	 * @param conf
+	 * @param inFile
+	 * @return
+	 * @throws IOException
+	 */
+	public static String copyFile(Configuration conf, Path inFile)
+			throws IOException {
+		FileSystem fs = inFile.getFileSystem(conf);
+		return copyFile(conf, fs.getFileStatus(inFile));
+	}
+
+	/**
+	 * Writes paths to a file where each path is a line.
+	 * 
+	 * @author ibrahimsabek
+	 * @param paths
+	 */
+	public static Path writePathsToFile(OperationsParams params, Path[] paths) {
+		String tmpFileName = "pathsDictionary.txt";
+		File tempFile;
+		try {
+			// store the dictionary of paths in a local file
+			tempFile = new File(tmpFileName);
+			Path localFilePath = new Path(tempFile.getAbsolutePath());
+			FileOutputStream outStream = new FileOutputStream(tempFile);
+			BufferedWriter buffWriter = new BufferedWriter(
+					new OutputStreamWriter(outStream));
+
+			for (int i = 0; i < paths.length; i++) {
+				buffWriter.write(paths[i].toString());
+				buffWriter.newLine();
+			}
+			buffWriter.close();
+			outStream.close();
+
+			// copy the local dictionary into an hdfs file
+			Configuration conf = new Configuration();
+			FileSystem fs = params.getPaths()[0].getFileSystem(conf);
+			Path hdfsFilePath = new Path(params.getPaths()[0].toString() + "/"
+					+ tmpFileName);
+
+			copyFromLocal(localFilePath, fs, hdfsFilePath);
+
+			return hdfsFilePath;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	/**
+	 * This function to copy a file from local file system to HDFS file stystem
+	 * 
+	 * @author ibrahimsabek
+	 * @param localPath
+	 * @param hdfsPath
+	 * @throws IOException
+	 */
+	private static void copyFromLocal(Path localPath, FileSystem hdfsFS,
+			Path hdfsPath) throws IOException {
+		FSDataOutputStream out = hdfsFS.create(hdfsPath);
+		FileInputStream localInputStream = new FileInputStream(new File(
+				localPath.toString()));
+		int bytesRead = 0;
+		byte[] localBuffer = new byte[1024];
+		while ((bytesRead = localInputStream.read(localBuffer)) > 0) {
+			out.write(localBuffer, 0, bytesRead);
+		}
+
+		localInputStream.close();
+		out.close();
+	}
 }
