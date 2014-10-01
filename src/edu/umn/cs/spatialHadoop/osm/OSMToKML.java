02c02870f9936f386eb7351fd97d4bbde34b021c
Ahmed Eldawy <eldawy@cs.umn.edu>
2014-09-30 20:08:22 -0500

    Moved OSM Polygon to KML logic to the OSMToKML operations

--- src/edu/umn/cs/spatialHadoop/osm/OSMToKML.java
+++ src/edu/umn/cs/spatialHadoop/osm/OSMToKML.java
@@ -15,6 +15,19 @@ import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.mapred.FileSplit;
 import org.apache.hadoop.util.GenericOptionsParser;
 
+import com.esri.core.geometry.Line;
+import com.esri.core.geometry.MultiPath;
+import com.esri.core.geometry.Segment;
+import com.esri.core.geometry.SpatialReference;
+import com.esri.core.geometry.ogc.OGCGeometry;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryCollection;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.LinearRing;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+
 import edu.umn.cs.spatialHadoop.OperationsParams;
 import edu.umn.cs.spatialHadoop.core.Rectangle;
 import edu.umn.cs.spatialHadoop.core.Shape;
@@ -27,6 +40,86 @@ import edu.umn.cs.spatialHadoop.mapred.ShapeArrayRecordReader;
  */
 public class OSMToKML {
 
+  
+  public static void geometryToKML(StringBuilder str, Geometry geom) {
+    if (geom instanceof Point) {
+      Point pt = (Point) geom;
+      str.append("<Point>");
+      str.append("<coordinates>");
+      str.append(pt.getX()+","+pt.getY());
+      str.append("</coordinates>");
+      str.append("</Point>");
+    } else if (geom instanceof LinearRing) {
+      LinearRing linearRing = (LinearRing) geom;
+      str.append("<LinearRing>");
+      str.append("<coordinates>");
+      for (Coordinate coord : linearRing.getCoordinates())
+        str.append(coord.x+","+coord.y+" ");
+      str.append("</coordinates>");
+      str.append("</LinearRing>");
+    } else if (geom instanceof LineString) {
+      LineString linestring = (LineString) geom;
+      str.append("<LineString>");
+      str.append("<coordinates>");
+      for (Coordinate coord : linestring.getCoordinates())
+        str.append(coord.x+","+coord.y+" ");
+      str.append("</coordinates>");
+      str.append("</LineString>");
+    } else if (geom instanceof Polygon) {
+      Polygon polygon = (Polygon) geom;
+      str.append("<Polygon>");
+
+      // Write exterior boundary
+      str.append("<outerBoundaryIs>");
+      geometryToKML(str, polygon.getExteriorRing());
+      str.append("</outerBoundaryIs>");
+
+      // Write all interior boundaries
+      for (int n = 0; n < polygon.getNumInteriorRing(); n++) {
+        str.append("<innerBoundaryIs>");
+        geometryToKML(str, polygon.getInteriorRingN(n));
+        str.append("</innerBoundaryIs>");
+      }
+      str.append("</Polygon>");
+    } else if (geom instanceof GeometryCollection) {
+      GeometryCollection geomCollection = (GeometryCollection) geom;
+      str.append("<GeometryCollection>");
+      for (int n = 0; n < geomCollection.getNumGeometries(); n++) {
+        geometryToKML(str, geomCollection.getGeometryN(n));
+      }
+      str.append("</GeometryCollection>");
+    } else {
+      throw new RuntimeException("Cannot generate KML from '"+geom.getClass()+"'");
+    }
+  }
+  
+  /**
+   * Returns a KML representation of this object.
+   * @return
+   */
+  public static String OSMtoKMLElement(OSMPolygon osm) {
+    StringBuilder str = new StringBuilder();
+    str.append("<Placemark>");
+    if (osm.tags != null) {
+      if (osm.tags.containsKey("name")) {
+        str.append("<name>");
+        str.append("<![CDATA[");
+        str.append(osm.tags.get("name"));
+        str.append("]]>");
+        str.append("</name>");
+      }
+      str.append("<description>");
+      str.append("<![CDATA[");
+      str.append(osm.tags.toString());
+      str.append("]]>");
+      str.append("</description>");
+      
+      geometryToKML(str, osm.geom);
+    }
+    str.append("</Placemark>");
+    return str.toString();
+  }
+  
   /**
    * @param args
    * @throws IOException 
@@ -54,7 +147,7 @@ public class OSMToKML {
     while (in.next(key, values)) {
       for (Shape shape : (Shape[]) values.get()) {
         if (shape instanceof OSMPolygon) {
-          out.println(((OSMPolygon) shape).toKMLElement());
+          out.println(OSMtoKMLElement((OSMPolygon) shape));
         }
       }
       out.println();
