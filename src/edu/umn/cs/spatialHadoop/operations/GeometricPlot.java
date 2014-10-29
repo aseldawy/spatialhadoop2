87f30bc970aae20186aa46e2fb39096725f1c629
Christopher Jonathan <cjonathan@cs.umn.edu>
2014-10-29 14:34:30 -0500

    Merge Geometric Plot with Faster Scans

--- src/edu/umn/cs/spatialHadoop/operations/GeometricPlot.java
+++ src/edu/umn/cs/spatialHadoop/operations/GeometricPlot.java
@@ -43,7 +43,7 @@ import org.apache.hadoop.mapred.InputSplit;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.JobContext;
-//import org.apache.hadoop.mapred.LocalJobRunner;
+import org.apache.hadoop.mapred.LocalJobRunner;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
@@ -1250,8 +1250,8 @@ public class GeometricPlot {
 			// files
 			job.set("mapred.job.tracker", "local");
 			// Use multithreading too
-			//job.setInt(LocalJobRunner.LOCAL_MAX_MAPS, Runtime.getRuntime()
-				//	.availableProcessors());
+			job.setInt(LocalJobRunner.LOCAL_MAX_MAPS, Runtime.getRuntime()
+					.availableProcessors());
 		}
 
 		if (background) {
@@ -1634,6 +1634,7 @@ public class GeometricPlot {
 				}
 			}
 		}
+
 		return result;
 	}
 
