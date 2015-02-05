3110a019d9afd1acce5e52b6ff5d1e15f2ffdf37
Ahmed Eldawy <eldawy@cs.umn.edu>
2015-02-05 17:04:10 -0600

    Added the new copyright notice to all files

--- src/edu/umn/cs/spatialHadoop/io/RandomCompressedInputStream.java
+++ src/edu/umn/cs/spatialHadoop/io/RandomCompressedInputStream.java
@@ -1,9 +1,11 @@
-/*******************************************************************
- * Copyright (C) 2014 by Regents of the University of Minnesota.   *
- *                                                                 *
- * This Software is released under the Apache License, Version 2.0 *
- * http://www.apache.org/licenses/LICENSE-2.0                      *
- *******************************************************************/
+/***********************************************************************
+* Copyright (c) 2015 by Regents of the University of Minnesota.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Apache License, Version 2.0 which 
+* accompanies this distribution and is available at
+* http://www.opensource.org/licenses/apache2.0.php.
+*
+*************************************************************************/
 package edu.umn.cs.spatialHadoop.io;
 
 import java.io.IOException;
--- src/edu/umn/cs/spatialHadoop/util/JspSpatialHelper.java
+++ src/edu/umn/cs/spatialHadoop/util/JspSpatialHelper.java
@@ -1,3 +1,11 @@
+/***********************************************************************
+* Copyright (c) 2015 by Regents of the University of Minnesota.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Apache License, Version 2.0 which 
+* accompanies this distribution and is available at
+* http://www.opensource.org/licenses/apache2.0.php.
+*
+*************************************************************************/
 package edu.umn.cs.spatialHadoop.util;
 
 import java.io.File;
