0a352044743eba5041e2f3d0e0704a1a70244ccd
Ahmed Eldawy <eldawy@cs.umn.edu>
2015-02-15 13:20:04 -0600

    Initial implementation of KdTreePartitioner (not tested yet)

---
+++ src/edu/umn/cs/spatialHadoop/core/KdTreePartitioner.java
@@ -0,0 +1,247 @@
+/***********************************************************************
+* Copyright (c) 2015 by Regents of the University of Minnesota.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Apache License, Version 2.0 which 
+* accompanies this distribution and is available at
+* http://www.opensource.org/licenses/apache2.0.php.
+*
+*************************************************************************/
+package edu.umn.cs.spatialHadoop.core;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Queue;
+import java.util.Vector;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.FileSplit;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import edu.umn.cs.spatialHadoop.OperationsParams;
+import edu.umn.cs.spatialHadoop.mapred.ShapeIterRecordReader;
+import edu.umn.cs.spatialHadoop.mapred.SpatialRecordReader.ShapeIterator;
+import edu.umn.cs.spatialHadoop.operations.Sampler;
+import edu.umn.cs.spatialHadoop.util.FileUtil;
+
+/**
+ * A partitioner that partitioner data using a K-d tree-based partitioner.
+ * @author Ahmed Eldawy
+ *
+ */
+public class KdTreePartitioner extends Partitioner {
+  private static final Log LOG = LogFactory.getLog(KdTreePartitioner.class);
+
+  /**MBR of the input file*/
+  private Rectangle mbr;
+  
+  /**
+   * Location of all splits stored in a complete binary tree which is encoded in
+   * a single array in a heap-like structure.
+   */
+  private double[] splits;
+  
+  /**
+   * A default constructor to be able to dynamically instantiate it
+   * and deserialize it
+   */
+  public KdTreePartitioner() {
+  }
+
+  /**
+   * Constructs a new grid partitioner which is used for indexing
+   * @param inPath
+   * @param job
+   * @throws IOException 
+   */
+  public static KdTreePartitioner createIndexingPartitioner(Path inPath,
+      Path outPath, JobConf job, boolean replicate) throws IOException {
+    Rectangle inMBR = (Rectangle) OperationsParams.getShape(job, "mbr");
+    
+    // Determine number of partitions
+    long inSize = FileUtil.getPathSize(inPath.getFileSystem(job), inPath);
+    FileSystem outFS = outPath.getFileSystem(job);
+    long outBlockSize = outFS.getDefaultBlockSize(outPath);
+    int partitions = (int) (inSize / outBlockSize);
+    LOG.info("K-d tree partitiong into "+partitions+" partitions");
+    
+    // Draw a random sample of the input file
+    final Vector<Point> vsample = new Vector<Point>();
+    
+    float sample_ratio = job.getFloat(SpatialSite.SAMPLE_RATIO, 0.01f);
+    long sample_size = job.getLong(SpatialSite.SAMPLE_SIZE, 100 * 1024 * 1024);
+
+    LOG.info("Reading a sample of "+(int)Math.round(sample_ratio*100) + "%");
+    ResultCollector<Point> resultCollector = new ResultCollector<Point>(){
+      @Override
+      public void collect(Point value) {
+        vsample.add(value.clone());
+      }
+    };
+    OperationsParams params2 = new OperationsParams(job);
+    params2.setFloat("ratio", sample_ratio);
+    params2.setLong("size", sample_size);
+    params2.setClass("outshape", Point.class, Shape.class);
+    Sampler.sample(new Path[] {inPath}, resultCollector, params2);
+    // Convert to array to be able to sort subranges
+    Point[] points = vsample.toArray(new Point[vsample.size()]);
+    vsample.clear();
+    LOG.info("Finished reading a sample of "+points.length+" records");
+
+    KdTreePartitioner kdp = createFromPoints(inMBR, partitions, points);
+    
+    return kdp;
+  }
+
+  private static KdTreePartitioner createFromPoints(Rectangle inMBR, int partitions,
+      Point[] points) {
+    // Keep splitting the space into halves until we reach the desired number of
+    // partitions
+    @SuppressWarnings("unchecked")
+    Comparator<Point>[] comparators = new Comparator[] {
+      new Comparator<Point>() {
+        @Override
+        public int compare(Point a, Point b) {
+          return a.x < b.x? -1 : (a.x > b.x? 1 : 0);
+        }},
+      new Comparator<Point>() {
+        @Override
+        public int compare(Point a, Point b) {
+          return a.y < b.y? -1 : (a.y > b.y? 1 : 0);
+        }}
+    };
+    
+    class SplitTask {
+      int fromIndex;
+      int toIndex;
+      int direction;
+      int partitionID;
+
+      /**Constructor using all fields*/
+      public SplitTask(int fromIndex, int toIndex, int direction,
+          int partitionID) {
+        this.fromIndex = fromIndex;
+        this.toIndex = toIndex;
+        this.direction = direction;
+        this.partitionID = partitionID;
+      }
+      
+    }
+    Queue<SplitTask> splitTasks = new ArrayDeque<SplitTask>();
+    splitTasks.add(new SplitTask(0, points.length, 0, 1));
+    
+    KdTreePartitioner kdp = new KdTreePartitioner();
+    kdp.mbr = new Rectangle(inMBR);
+    kdp.splits = new double[partitions+1];
+    
+    while (!splitTasks.isEmpty()) {
+      SplitTask splitTask = splitTasks.remove();
+      quickMedian(points, splitTask.fromIndex, splitTask.toIndex,
+          comparators[splitTask.direction]);
+      Point median = points[(splitTask.fromIndex + splitTask.toIndex)/2];
+      kdp.splits[splitTask.partitionID] = splitTask.direction == 0 ?
+          median.x : median.y;
+      if (splitTask.partitionID * 2 < partitions) {
+        splitTasks.add(new SplitTask(splitTask.fromIndex,
+            (splitTask.fromIndex + splitTask.toIndex) / 2,
+            1 - splitTask.direction, splitTask.partitionID * 2));
+      }
+      if (splitTask.partitionID * 2 + 1 < partitions) {
+        splitTasks.add(new SplitTask(
+            (splitTask.fromIndex + splitTask.toIndex) / 2, splitTask.toIndex,
+            1 - splitTask.direction, splitTask.partitionID * 2 + 1));
+      }
+    }
+    return kdp;
+  }
+
+  /**
+   * Reorders the given array so that the element at (fromIndex+toIndex)/2
+   * points to the median of this subrange according to the given comparator.
+   * If the size of the given subrange is even (i.e., toIndex-fromIndex is a
+   * multiple of two), then there are two values that need to be interpolated
+   * to give the median, in this case, these two values will be placed in
+   * the indexes (fromIndex+toIndex)/2-1 and (fromIndex+toIndex)/2.
+   * @param a
+   * @param fromIndex
+   * @param toIndex
+   * @param c
+   */
+  public static <T> void quickMedian(T[] a, int fromIndex, int toIndex,
+      Comparator<T> c) {
+    Arrays.sort(a, fromIndex, toIndex, c);
+  }
+
+  @Override
+  public void write(DataOutput out) throws IOException {
+    mbr.write(out);
+    out.writeInt(splits.length);
+    ByteBuffer bbuffer = ByteBuffer.allocate(splits.length * 8);
+    for (double split : splits)
+      bbuffer.putDouble(split);
+    if (bbuffer.hasRemaining())
+      throw new RuntimeException("Did not calculate buffer size correctly");
+    out.write(bbuffer.array(), bbuffer.arrayOffset(), bbuffer.position());
+  }
+
+  @Override
+  public void readFields(DataInput in) throws IOException {
+    if (mbr == null)
+      mbr = new Rectangle();
+    mbr.readFields(in);
+    int partitions = in.readInt();
+    splits = new double[partitions];
+    
+    int bufferLength = splits.length * 8;
+    byte[] buffer = new byte[bufferLength];
+    in.readFully(buffer);
+    ByteBuffer bbuffer = ByteBuffer.wrap(buffer);
+    for (int i = 0; i < splits.length; i++)
+      splits[i] = bbuffer.getDouble();
+    if (bbuffer.hasRemaining())
+      throw new RuntimeException("Error reading STR partitioner");
+  }
+  
+  @Override
+  public int getPartitionCount() {
+    return splits.length - 1; // First element is dummy
+  }
+
+  @Override
+  public void overlapPartitions(Shape shape, ResultCollector<Integer> matcher) {
+    throw new RuntimeException("Not implemented");
+  }
+
+  @Override
+  public CellInfo getPartition(int id) {
+    throw new RuntimeException("Not implemented");
+  }
+  
+  public static void main(String[] args) throws IOException {
+    OperationsParams params = new OperationsParams(new GenericOptionsParser(args));
+    
+    Path inPath = params.getInputPath();
+    long length = inPath.getFileSystem(params).getFileStatus(inPath).getLen();
+    ShapeIterRecordReader reader = new ShapeIterRecordReader(params,
+        new FileSplit(inPath, 0, length, new String[0]));
+    Rectangle key = reader.createKey();
+    ShapeIterator shapes = reader.createValue();
+    final Vector<Point> points = new Vector<Point>();
+    while (reader.next(key, shapes)) {
+      for (Shape s : shapes) {
+        points.add(s.getMBR().getCenterPoint());
+      }
+    }
+    Rectangle inMBR = (Rectangle)OperationsParams.getShape(params, "mbr");
+    
+    KdTreePartitioner kdp = createFromPoints(inMBR, 5, points.toArray(new Point[points.size()]));
+  }
+}
