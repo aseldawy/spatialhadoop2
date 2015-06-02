71e4c58b09fff7a3144cc51552282b63ed5ed699
Ahmed Eldawy <eldawy@cs.umn.edu>
2015-06-01 19:34:40 -0500

    Added methods to sample compressed files

--- src/edu/umn/cs/spatialHadoop/operations/Sampler2.java
+++ src/edu/umn/cs/spatialHadoop/operations/Sampler2.java
@@ -14,10 +14,17 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Vector;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.CodecPool;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionCodecFactory;
+import org.apache.hadoop.io.compress.Decompressor;
+import org.apache.hadoop.io.compress.SplitCompressionInputStream;
+import org.apache.hadoop.io.compress.SplittableCompressionCodec;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.util.GenericOptionsParser;
 
@@ -61,7 +68,6 @@ public class Sampler2 {
         }
       }
     }
-    
     return sampleLocal(splits.toArray(new FileSplit[splits.size()]), ratioOrCount, output, conf);
   }
   
@@ -108,14 +114,13 @@ public class Sampler2 {
         sampledLines = 0;
         for (int iFile = i1; iFile < i2; iFile++) {
           try {
-            FileSystem fs = files[iFile].getPath().getFileSystem(conf);
             long randomSeed = conf.getLong("seed", System.currentTimeMillis()) + iFile;
             if (ratioOrCount > 1)
-              sampledLines += sampleFileSplitByCount(fs, files[iFile],
+              sampledLines += sampleFileSplitByCount(files[iFile], conf,
                   sampleSizePerFile[iFile], randomSeed, output);
             else
-              sampledLines += sampleFileSplitByRatio(fs, files[iFile],
-                  ratioOrCount, randomSeed, output);
+                  sampledLines += sampleFileSplitByRatio(files[iFile], conf,
+                      ratioOrCount, randomSeed, output);
           } catch (IOException e) {
             throw new RuntimeException("Error while sampling file "+files[iFile]);
           }
@@ -129,6 +134,81 @@ public class Sampler2 {
       totalSampledLines += actualSampleSize;
     return totalSampledLines;
   }
+
+  /**
+   * Sample from a stream by a sampling ratio. The stream has to be scanned
+   * and each record is selected with the a probability equal to the given
+   * sampling ratio.
+   * @param in
+   * @param streamLength
+   * @param ratio
+   * @param seed
+   * @param output
+   * @return
+   * @throws IOException
+   */
+  private static int sampleStreamByRatio(InputStream in,
+      double ratio, long seed, ResultCollector<Text> output) throws IOException {
+    Random rand = new Random(seed);
+    Text line = new Text2();
+    int sampleSize = 0;
+    while (readUntilEOL(in, line) > 0) {
+      if (rand.nextDouble() < ratio) {
+        if (output != null)
+          output.collect(line);
+        sampleSize++;
+      }
+      line.clear();
+    }
+    
+    return sampleSize;
+  }
+
+  /**
+   * Reads a sample of a specific count from a stream. With a stream, we cannot
+   * randomly seek to any position and we have to use reservoir sampling
+   * techniques.
+   * 
+   * @return
+   * @throws IOException
+   */
+  private static int sampleStreamByCount(InputStream in, long streamLength,
+      int count, long seed, ResultCollector<Text> output) throws IOException {
+    Random rand = new Random(seed);
+    
+    Text dummyLine = new Text2();
+    Text[] sample = new Text[count];
+    long pos = 0;
+    int k = 0;
+    
+    while (pos < streamLength) {
+      if (k < count) {
+        // Phase 1- Fill in the reservoir
+        pos += readUntilEOL(in, sample[k] = new Text2());
+      } else {
+        // Phase 2- Replace an existing item with probability p=(count/k)
+        if (rand.nextInt(k) < count) {
+          // Replace a randomly selected item
+          int victim = rand.nextInt(count);
+          sample[victim].clear();
+          pos += readUntilEOL(in, sample[victim]);
+        } else {
+          // Skip this item
+          dummyLine.clear();
+          pos += readUntilEOL(in, dummyLine);
+        }
+      }
+      k++;
+    }
+    // Report sampled items
+    int sampleSize = Math.min(k, count);
+    if (output != null) {
+      for (int i = 0; i < sampleSize; i++)
+        output.collect(sample[i]);
+    }
+    
+    return sampleSize;
+  }  
   
   /**
    * Sample a specific number of lines from a given file
@@ -140,38 +220,74 @@ public class Sampler2 {
    * @return
    * @throws IOException
    */
-  private static int sampleFileSplitByCount(FileSystem fs, FileSplit file,
+  private static int sampleFileSplitByCount(FileSplit file, Configuration conf,
       int count, long seed, ResultCollector<Text> output) throws IOException {
-    Text line = new Text2();
-    // Open the file and read the sample
-    InputStream in = fs.open(file.getPath());
-    long pos = 0; // Current position in file
-    int sampledLines = 0;
-
-    // Generate random offsets and keep them sorted for IO efficiency
-    Random rand = new Random(seed);
-    long[] sampleOffsets = new long[count];
-    for (int i = 0; i < count; i++)
-      sampleOffsets[i] = Math.abs(rand.nextLong()) % file.getLength() + file.getStart();
-    Arrays.sort(sampleOffsets);
-    
-    // Sample the generated numbers
-    for (int i = 0; i < count; i++) {
-      pos += in.skip(sampleOffsets[i] - pos);
-      // Skip until end of line
-      line.clear();
-      pos += readUntilEOL(in, line);
-      // Read the next full line
-      line.clear();
-      if ((pos += readUntilEOL(in, line)) > 1) {
-        sampledLines++;
-        if (output != null)
-          output.collect(line);
+    InputStream in = null;
+    Decompressor decompressor = null;
+    try {
+      CompressionCodecFactory compressionCodecFactory = new CompressionCodecFactory(conf);
+      CompressionCodec codec = compressionCodecFactory.getCodec(file.getPath());
+      
+      // Open the file and read the sample
+      FileSystem fs = file.getPath().getFileSystem(conf);
+      in = fs.open(file.getPath());
+      int sampledLines = 0;
+      
+      if (codec != null) {
+        // Special handling for compressed file as we cannot compute the actual
+        // size of the underlying data
+        decompressor = CodecPool.getDecompressor(codec);
+        
+        if (codec instanceof SplittableCompressionCodec) {
+          // A splittable compression codec, can seek to the desired input pos
+          final SplitCompressionInputStream cIn =
+              ((SplittableCompressionCodec)codec).createInputStream(
+                  in, decompressor, file.getStart(), file.getStart() + file.getLength(),
+                  SplittableCompressionCodec.READ_MODE.BYBLOCK);
+          in = cIn;
+          // Adjust the start of the end based on the compressed data
+          long start = cIn.getAdjustedStart();
+          long end = cIn.getAdjustedEnd();
+          sampledLines = sampleStreamByCount(in, end - start, count, seed, output);
+        } else {
+          // Non-splittable input, need to start from the beginning
+          in = codec.createInputStream(in, decompressor);
+          sampledLines = sampleStreamByCount(in, Long.MAX_VALUE, count, seed, output);
+        }
+      } else {
+        long pos = 0; // Current position in file
+        
+        // Generate random offsets and keep them sorted for IO efficiency
+        Random rand = new Random(seed);
+        long[] sampleOffsets = new long[count];
+        for (int i = 0; i < count; i++)
+          sampleOffsets[i] = Math.abs(rand.nextLong()) % file.getLength() + file.getStart();
+        Arrays.sort(sampleOffsets);
+        
+        // Sample the generated numbers
+        Text line = new Text2();
+        for (int i = 0; i < count; i++) {
+          pos += in.skip(sampleOffsets[i] - pos);
+          // Skip until end of line
+          line.clear();
+          pos += readUntilEOL(in, line);
+          // Read the next full line
+          line.clear();
+          if ((pos += readUntilEOL(in, line)) > 1) {
+            sampledLines++;
+            if (output != null)
+              output.collect(line);
+          }
+        }
       }
+      
+      return sampledLines;
+    } finally {
+      if (in != null)
+        in.close();
+      if (decompressor != null)
+        CodecPool.returnDecompressor(decompressor);
     }
-
-    in.close();
-    return sampledLines;
   }
   
   /**
@@ -184,53 +300,101 @@ public class Sampler2 {
    * @return
    * @throws IOException
    */
-  private static int sampleFileSplitByRatio(FileSystem fs, FileSplit file,
+  private static int sampleFileSplitByRatio(FileSplit file,
+      Configuration conf,
       float ratio, long seed, ResultCollector<Text> output) throws IOException {
+    
+    InputStream in = null;
+    Decompressor decompressor = null;
+    int sampledLines;
     Text line = new Text2();
-    // Open the file and read the sample
-    InputStream in = fs.open(file.getPath());
-    long pos = 0; // Current position in file
-    if (file.getStart() > 0) {
-      pos += in.skip(file.getStart());
-      pos += readUntilEOL(in, line);
-    }
     
-    // Initialize the random variable which is used for sampling
-    Random rand = new Random(seed);
+    try {
+      CompressionCodecFactory compressionCodecFactory = new CompressionCodecFactory(conf);
+      CompressionCodec codec = compressionCodecFactory.getCodec(file.getPath());
+      FileSystem fs = file.getPath().getFileSystem(conf);
+      in = fs.open(file.getPath());
+      
+      if (codec != null) {
+        // Special handling for compressed file as we cannot compute the actual
+        // size of the underlying data
+        decompressor = CodecPool.getDecompressor(codec);
+        
+        if (codec instanceof SplittableCompressionCodec) {
+          // A splittable compression codec, can seek to the desired input pos
+          final SplitCompressionInputStream cIn =
+              ((SplittableCompressionCodec)codec).createInputStream(
+                  in, decompressor, file.getStart(), file.getStart() + file.getLength(),
+                  SplittableCompressionCodec.READ_MODE.BYBLOCK);
+          in = cIn;
+          // Adjust the start of the end based on the compressed data
+          long start = cIn.getAdjustedStart();
+          long end = cIn.getAdjustedEnd();
+          // Skip first line if needed
+          if (file.getStart() > 0)
+            start += readUntilEOL(cIn, line);
+          
+          sampledLines = sampleStreamByRatio(in, ratio, seed, output);
+        } else {
+          // Non-splittable input, need to start from the beginning
+          in = codec.createInputStream(in, decompressor);
+          // No need to skip first line because we actually read the file from
+          // the beginning
+          sampledLines = sampleStreamByRatio(in, ratio, seed, output);
+        }
+      } else {
+        // Not a compressed file. Apply a more efficient, though approximate,
+        // solution
+        // Open the file and read the sample
+        long pos = 0; // Current position in file
+        if (file.getStart() > 0) {
+          pos += in.skip(file.getStart());
+          pos += readUntilEOL(in, line);
+        }
+        
+        // Initialize the random variable which is used for sampling
+        Random rand = new Random(seed);
+        sampledLines = 0;
 
-    // Read the first 10 lines to estimate the average record size
-    int sampledLines = 0;
-    long end = file.getStart() + file.getLength();
-    for (int i = 0; i < 10 && pos < end; i++) {
-      line.clear();
-      pos += readUntilEOL(in, line);
-      if (rand.nextFloat() < ratio) {
-        sampledLines++;
-        if (output != null)
-          output.collect(line);
-      }
-    }
-    
-    int averageLineSize = (int) ((pos - file.getStart()) / 10);
-    int count = Math.round(ratio * file.getLength() / averageLineSize) - sampledLines;
-    long[] sampleOffsets = new long[count];
-    for (int i = 0; i < count; i++)
-      sampleOffsets[i] = Math.abs(rand.nextLong()) % (end - pos) + file.getStart();
-    Arrays.sort(sampleOffsets);
+        // Read the first 10 lines to estimate the average record size
+        long end = file.getStart() + file.getLength();
+        for (int i = 0; i < 10 && pos < end; i++) {
+          line.clear();
+          pos += readUntilEOL(in, line);
+          if (rand.nextFloat() < ratio) {
+            sampledLines++;
+            if (output != null)
+              output.collect(line);
+          }
+        }
+        
+        int averageLineSize = (int) ((pos - file.getStart()) / 10);
+        int count = Math.round(ratio * file.getLength() / averageLineSize) - sampledLines;
+        long[] sampleOffsets = new long[count];
+        for (int i = 0; i < count; i++)
+          sampleOffsets[i] = Math.abs(rand.nextLong()) % (end - pos) + file.getStart();
+        Arrays.sort(sampleOffsets);
 
-    // Sample the generated numbers
-    for (int i = 0; i < count; i++) {
-      pos += in.skip(sampleOffsets[i] - pos);
-      // Skip until end of line
-      line.clear();
-      pos += readUntilEOL(in, line);
-      // Read the next full line
-      line.clear();
-      if ((pos += readUntilEOL(in, line)) > 1) {
-        sampledLines++;
-        if (output != null)
-          output.collect(line);
+        // Sample the generated numbers
+        for (int i = 0; i < count; i++) {
+          pos += in.skip(sampleOffsets[i] - pos);
+          // Skip until end of line
+          line.clear();
+          pos += readUntilEOL(in, line);
+          // Read the next full line
+          line.clear();
+          if ((pos += readUntilEOL(in, line)) > 1) {
+            sampledLines++;
+            if (output != null)
+              output.collect(line);
+          }
+        }
       }
+    } finally {
+      if (in != null)
+        in.close();
+      if (decompressor != null)
+        CodecPool.returnDecompressor(decompressor);
     }
 
     in.close();
@@ -246,28 +410,32 @@ public class Sampler2 {
    * @throws IOException 
    */
   private static int readUntilEOL(InputStream in, Text line) throws IOException {
-    final byte[] lineBytes = new byte[1024];
-    int length = 0;
+    final byte[] bufferBytes = new byte[1024];
+    int bufferLength = 0; // Length of the buffer
     do {
-      if (length == lineBytes.length) {
-        line.append(lineBytes, 0, length);
-        length = 0;
+      if (bufferLength == bufferBytes.length) {
+        // Buffer full. Copy to the output text
+        line.append(bufferBytes, 0, bufferLength);
+        bufferLength = 0;
       }
-      if (length == 0) {
+      if (bufferLength == 0) {
         // Read and skip any initial EOL characters
         do {
-          lineBytes[0] = (byte) in.read();
-        } while (lineBytes[0] != -1 &&
-            (lineBytes[0] == '\n' || lineBytes[0] == '\r'));
-        if (lineBytes[0] != -1)
-          length++;
+          bufferBytes[0] = (byte) in.read();
+        } while (bufferBytes[0] != -1 &&
+            (bufferBytes[0] == '\n' || bufferBytes[0] == '\r'));
+        if (bufferBytes[0] != -1)
+          bufferLength++;
       } else {
-        lineBytes[length++] = (byte) in.read();
+        bufferBytes[bufferLength++] = (byte) in.read();
       }
-    } while (lineBytes[length-1] != -1 &&
-        lineBytes[length-1] != '\n' && lineBytes[length-1] != '\r');
-    length--;
-    line.append(lineBytes, 0, length);
+    } while (bufferLength > 0 &&
+        bufferBytes[bufferLength-1] != -1 &&
+        bufferBytes[bufferLength-1] != '\n' && bufferBytes[bufferLength-1] != '\r');
+    if (bufferLength > 0) {
+      bufferLength--;
+      line.append(bufferBytes, 0, bufferLength);
+    }
     return line.getLength();
   }
 
