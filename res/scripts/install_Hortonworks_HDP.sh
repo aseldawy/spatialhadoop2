1772a080ade6f4e36799b4621c68aaf1518c3552
Stephen Knox <stephenknox73@gmail.com>
2015-08-29 16:39:23 +0000

    Add script to install spatialhadoop jar to cluster

---
+++ res/scripts/install_Hortonworks_HDP.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+$HDP_VERSION = 2.2.6.0-2800
+
+mkdir spatialhadoop2
+cd spatialhadoop2
+wget http://spatialhadoop.cs.umn.edu/downloads/spatialhadoop-2.3-2.tar.gz
+tar -xzvf spatialhadoop-2.3-2.tar.gz
+
+sudo cp bin/* /usr/bin 
+sudo cp etc/hadoop/conf/* /etc/hadoop/conf
+sudo cp -r share/hadoop/common/lib/* /usr/local/lib/hadoop/lib
+sudo cp spatialhadoop-main.jar /usr/hdp/$HDP_VERSION/hadoop-mapreduce
