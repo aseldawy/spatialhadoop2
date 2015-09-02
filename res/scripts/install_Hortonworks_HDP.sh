8628ac853a02c4b2e4dd050030a6be59faab12c3
Stephen Knox <stephenknox73@gmail.com>
2015-09-02 22:58:23 +0000

    improved script to be parametized

--- res/scripts/install_Hortonworks_HDP.sh
+++ res/scripts/install_Hortonworks_HDP.sh
@@ -3,13 +3,15 @@
 # This script installs the redistributable package (in this case spatialhadoop-2.3-2 onto a Hortonworks HDP cluster)
 
 $HDP_VERSION="2.2.6.0-2800"
+$SPATIALHADOOP_VERSION="spatialhadoop-2.4-rc1.jar"
 
 mkdir spatialhadoop2
 cd spatialhadoop2
-wget http://spatialhadoop.cs.umn.edu/downloads/spatialhadoop-2.3-2.tar.gz
-tar -xzvf spatialhadoop-2.3-2.tar.gz
+wget http://spatialhadoop.cs.umn.edu/downloads/$SPATIALHADOOP_VERSION
+tar -xzvf $SPATIALHADOOP_VERSION
 
 sudo cp bin/* /usr/bin 
 sudo cp etc/hadoop/conf/* /etc/hadoop/conf
 sudo cp -r share/hadoop/common/lib/* /usr/local/lib/hadoop/lib
+sudo cp share/hadoop/common/lib/spatialhadoop-2.4-rc1.jar /usr/hdp/$HDP_VERSION/hadoop/lib/
 sudo cp spatialhadoop-main.jar /usr/hdp/$HDP_VERSION/hadoop-mapreduce
