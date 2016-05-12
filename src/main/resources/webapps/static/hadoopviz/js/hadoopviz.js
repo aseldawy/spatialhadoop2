fd0a64ebf4b01b537a916b60f17516dd3c76a29d
Ahmed Eldawy <eldawy@cs.umn.edu>
2016-05-11 21:13:09 -0500

    Display the image and global index in a nice way

--- src/main/resources/webapps/static/hadoopviz/js/hadoopviz.js
+++ src/main/resources/webapps/static/hadoopviz/js/hadoopviz.js
@@ -23,9 +23,6 @@ $(function() {
       });
       
       // If it is a dataset, further visualize its contents
-      if (data["image"] != null) {
-        // There is an image, display it
-      }
       if (data["master"] != null) {
         // There is a master file, display it
         var masterFilePath = path+"/"+data["master"];
@@ -35,14 +32,27 @@ $(function() {
           method: "GET",
           dataType: "text",
           processData: false
-        }).success(function(data) {
-          var masterJSON = convertCSVToJSON(data);
-          dust.render("global-index-template", masterJSON, function(err, out) {
+        }).success(function(gindex_csv) {
+          var gindex_json = convertCSVToJSON(gindex_csv);
+          gindex_json["Image"] = path+"/"+data["image"];
+          dust.render("global-index-template", gindex_json, function(err, out) {
             jQuery("#global-index").html(out);
           });
         });
       } else {
-        jQuery("#global-index").html("");
+        if (data["image"] != null) {
+          // There is an image, display it
+          var gindex_json = {
+            Image: path+"/"+data["image"],
+            ImageWidth: jQuery("#global-index").width(),
+            ImageHeight: jQuery(window).height()
+          };
+          dust.render("global-index-template", gindex_json, function(err, out) {
+            jQuery("#global-index").html(out);
+          });
+        } else {
+          jQuery("#global-index").html("");
+        }        
       }
     }).error(function(xhr, data) {alert(data);});
   }
@@ -50,6 +60,9 @@ $(function() {
   dust.loadSource(dust.compile($('#file-list-template').html(), 'file-list-template'));
   dust.loadSource(dust.compile($('#global-index-template').html(), 'global-index-template'));
   
+  /**
+   * Update the contents of the current file from the hash list.
+   */
   function updateFromHash() {
     var path = window.location.hash.slice(1);
     if (path.length == 0)
@@ -58,6 +71,9 @@ $(function() {
     listFiles(path);
   }
   
+  /**
+   * Takes the size in bytes and returns a human readable string.
+   */
   function humanReadableSize(size) {
     var units = [ "", "KB", "MB", "GB", "TB", "PB" ];
     var unit = 0;
@@ -68,6 +84,9 @@ $(function() {
     return size.toFixed(2) + " "+ units[unit];
   }
   
+  /**
+   * Formats the date to appear in the file list as it does with HDFS command
+   */
   function formatDate(date) {
     date = new Date(date);
     var year = date.getFullYear();
@@ -78,17 +97,30 @@ $(function() {
     return year+"-"+month+"-"+day+" "+hours+":"+minutes;
   }
   
+  /**
+   * Adds a Dust helper that formats the date
+   */
   dust.helpers.formatDateHelper = function (chunk, context, bodies, params) {
     var value = dust.helpers.tap(params.value, chunk, context);
     return chunk.write(formatDate(parseInt(value)));
   };
 
+  /**
+   * Adds a Dust helper that formats the size in human readable form
+   */
   dust.helpers.formatSizeHelper = function (chunk, context, bodies, params) {
     var value = dust.helpers.tap(params.value, chunk, context);
     return chunk.write(humanReadableSize(parseInt(value)));
   };
   
+  /**
+   * Convert the global index from CSV to JSON format to feed to Dust.
+   */
   function convertCSVToJSON(csv) {
+    var screen_width = jQuery("#global-index").width();
+    var screen_height = jQuery(window).height();
+    
+    // Convert the data to JSON and calculate the MBR of the input
     var lines = csv.split("\n");
     var json_lines = [];
     var minx = Infinity, miny = Infinity,
@@ -116,13 +148,42 @@ $(function() {
           maxy = this_line["y2"];
       }
     }
+    
+    // Scale all partitions to fill the screen
+    // Notice that we could use SVG scale functionality, however, it scales
+    // the stroke-width as well which we do not wish to scale.
+    // Scaling the stroke-width might cause inputs with very large domain
+    // to produce rectangles with very thin strokes, and similarly, inputs
+    // with very small domain to produce rectangles with very thick strokes.
+    if (((maxx-minx) / (maxy-miny)) > (screen_width/screen_height)) {
+      // Keep width fixed and change height
+      screen_height = screen_width * (maxy-miny) / (maxx-minx);
+    } else {
+      // Keep height fixed and change width
+      screen_width = screen_height * (maxx-minx) / (maxy-miny);
+    }
+    for (var i = 0; i < json_lines.length; i++) {
+      var json_line = json_lines[i];
+      // Scale x-dimension to be in the range [0, screen_width)
+      json_line["x1"] = (json_line["x1"] - minx) * screen_width / (maxx-minx);
+      json_line["x2"] = (json_line["x2"] - minx) * screen_width / (maxx-minx);
+      // Scale y-dimension to be in the range [0, screen_height)
+      // Also flip the records vertically to match the generated image
+      var y1 = (json_line["y1"] - miny) * screen_height / (maxy-miny);
+      var y2 = (json_line["y2"] - miny) * screen_height / (maxy-miny);
+      json_line["y2"] = screen_height - y1;
+      json_line["y1"] = screen_height - y2;
+    }
+    
     return {
       Partitions: json_lines,
-      minx: minx, miny: miny,
-      maxx: maxx, maxy: maxy
+      ImageWidth: screen_width, ImageHeight: screen_height
     };
   }
   
+  // Register address change of the URL
   jQuery(window).bind('hashchange', updateFromHash);
+  
+  // Initially, use the URL to initially list the files
   updateFromHash();
 });
