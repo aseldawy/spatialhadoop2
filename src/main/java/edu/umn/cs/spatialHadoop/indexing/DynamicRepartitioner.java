7a49315e8a5e7a88cda39a3bd6309c22a5b5602a
tinvukhac <tinvukhac@gmail.com>
2017-01-26 13:47:23 -0800

    repartition job

--- src/main/java/edu/umn/cs/spatialHadoop/indexing/DynamicRepartitioner.java
+++ src/main/java/edu/umn/cs/spatialHadoop/indexing/DynamicRepartitioner.java
@@ -11,8 +11,15 @@ import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.PathFilter;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.ClusterStatus;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.LocalJobRunner;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.util.LineReader;
 
@@ -20,15 +27,86 @@ import edu.umn.cs.spatialHadoop.OperationsParams;
 import edu.umn.cs.spatialHadoop.core.CellInfo;
 import edu.umn.cs.spatialHadoop.core.Rectangle;
 import edu.umn.cs.spatialHadoop.core.ResultCollector;
+import edu.umn.cs.spatialHadoop.core.Shape;
 import edu.umn.cs.spatialHadoop.io.Text2;
+import edu.umn.cs.spatialHadoop.mapreduce.SpatialInputFormat3;
 import edu.umn.cs.spatialHadoop.operations.FileMBR;
 
 public class DynamicRepartitioner {
 	
 	private static final Log LOG = LogFactory.getLog(Indexer.class);
+	
+	/**
+	 * The map function that partitions the data using the configured
+	 * partitioner. Refer from Indexer class
+	 * 
+	 * @author Tin Vu
+	 *
+	 */
+	public static class DynamicRepartitionerMap extends Mapper<Rectangle, Iterable<? extends Shape>, IntWritable, Shape> {
+
+		/** The partitioner used to partitioner the data across reducers */
+		private Partitioner partitioner;
+		/**
+		 * Whether to replicate a record to all overlapping partitions or to
+		 * assign it to only one partition
+		 */
+		private boolean replicate;
+
+		@Override
+		protected void setup(Context context) throws IOException, InterruptedException {
+			super.setup(context);
+			this.partitioner = Partitioner.getPartitioner(context.getConfiguration());
+			this.replicate = context.getConfiguration().getBoolean("replicate", false);
+		}
+
+		@Override
+		protected void map(Rectangle key, Iterable<? extends Shape> shapes, final Context context)
+				throws IOException, InterruptedException {
+			final IntWritable partitionID = new IntWritable();
+			for (final Shape shape : shapes) {
+				if (replicate) {
+					partitioner.overlapPartitions(shape, new ResultCollector<Integer>() {
+						@Override
+						public void collect(Integer r) {
+							partitionID.set(r);
+							try {
+								context.write(partitionID, shape);
+							} catch (IOException e) {
+								LOG.warn("Error checking overlapping partitions", e);
+							} catch (InterruptedException e) {
+								LOG.warn("Error checking overlapping partitions", e);
+							}
+						}
+					});
+				} else {
+					partitionID.set(partitioner.overlapPartition(shape));
+					if (partitionID.get() >= 0)
+						context.write(partitionID, shape);
+				}
+				context.progress();
+			}
+		}
+	}
+
+	public static class DynamicRepartitionerReduce<S extends Shape> extends Reducer<IntWritable, Shape, IntWritable, Shape> {
+
+		@Override
+		protected void reduce(IntWritable partitionID, Iterable<Shape> shapes, Context context)
+				throws IOException, InterruptedException {
+			LOG.info("Working on partition #" + partitionID);
+			for (Shape shape : shapes) {
+				context.write(partitionID, shape);
+				context.progress();
+			}
+			// Indicate end of partition to close the file
+			context.write(new IntWritable(-partitionID.get() - 1), null);
+			LOG.info("Done with partition #" + partitionID);
+		}
+	}
 
 	private static Job repartitionMapReduce(Path inPath, OperationsParams params)
-			throws IOException, InterruptedException {
+			throws IOException, InterruptedException, ClassNotFoundException {
 		Job job = new Job(params, "DynamicRepartitioner");
 		Configuration conf = job.getConfiguration();
 		job.setJarByClass(DynamicRepartitioner.class);
@@ -73,14 +151,53 @@ public class DynamicRepartitioner {
 			long t2 = System.currentTimeMillis();
 			System.out.println("Total time for space subdivision in millis: " + (t2 - t1));
 			
-			ArrayList<PotentialPartition> potentialPartitions = getPotentialPartitions(inPath, partitioner, params);
+			ArrayList<PotentialPartition> partitionsToSplit = getPartitionsToSplit(inPath, partitioner, params);
+			
+			// Split partition
+			for(PotentialPartition pp: partitionsToSplit) {
+				FilePartitioner filePartitioner = new FilePartitioner();
+				for(IntersectionInfo intersection: pp.intersections) {
+					Partition tempPartition = new Partition();
+					tempPartition.set(intersection.getCell());
+					filePartitioner.partitions.add(tempPartition);
+				}
+				Partitioner.setPartitioner(conf, filePartitioner);
+				
+				// Set mapper and reducer
+			    Shape shape = OperationsParams.getShape(conf, "shape");
+			    job.setMapperClass(DynamicRepartitionerMap.class);
+			    job.setMapOutputKeyClass(IntWritable.class);
+			    job.setMapOutputValueClass(shape.getClass());
+			    job.setReducerClass(DynamicRepartitionerReduce.class);
+			    // Set input and output
+			    job.setInputFormatClass(SpatialInputFormat3.class);
+			    SpatialInputFormat3.setInputPaths(job, new Path(inPath, pp.filename));
+			    job.setOutputFormatClass(IndexOutputFormat.class);
+				IndexOutputFormat.setOutputPath(job, tempPath);
+			    // Set number of reduce tasks according to cluster status
+			    ClusterStatus clusterStatus = new JobClient(new JobConf()).getClusterStatus();
+			    job.setNumReduceTasks(Math.max(1, Math.min(partitioner.getPartitionCount(),
+			        (clusterStatus.getMaxReduceTasks() * 9) / 10)));
+
+			    // Use multithreading in case the job is running locally
+			    conf.setInt(LocalJobRunner.LOCAL_MAX_MAPS, Runtime.getRuntime().availableProcessors());
+			    
+			    // Start the job
+			    if (conf.getBoolean("background", false)) {
+			      // Run in background
+			      job.submit();
+			    } else {
+			      job.waitForCompletion(conf.getBoolean("verbose", false));
+			    }
+			}
 		}
 
 		return job;
 	}
 
-	private static ArrayList<PotentialPartition> getPotentialPartitions(Path inPath, final Partitioner partitioner, OperationsParams params) throws IOException {
+	private static ArrayList<PotentialPartition> getPartitionsToSplit(Path inPath, final Partitioner partitioner, OperationsParams params) throws IOException {
 		final ArrayList<PotentialPartition> potentialPartitions = new ArrayList<PotentialPartition>();
+		ArrayList<PotentialPartition> partitionsToSplit = new ArrayList<PotentialPartition>();
 		ArrayList<Partition> currentPartitions = new ArrayList<Partition>();
 		
 		Configuration conf = new Configuration();
@@ -119,16 +236,20 @@ public class DynamicRepartitioner {
 		
 		// Iterate the list of potential partitions to get the partitions to split
 		for(PotentialPartition pp: potentialPartitions) {
+			boolean keep = false;
 			for(IntersectionInfo intersection: pp.intersections) {
-				System.out.println("ID = " + intersection.getCell().cellId);
-				System.out.println("JS = " + intersection.getJsValue());
+//				System.out.println("ID = " + intersection.getCell().cellId);
+//				System.out.println("JS = " + intersection.getJsValue());
 				if(intersection.getJsValue() >= jsimValue) {
-					System.out.println("is potential");
+					keep = true;
 				}
+			}
+			if(!keep) {
+				partitionsToSplit.add(pp);
 			}
 		}
 		
-		return potentialPartitions;
+		return partitionsToSplit;
 	}
 
 	private static void printUsage() {
@@ -138,7 +259,7 @@ public class DynamicRepartitioner {
 		GenericOptionsParser.printGenericCommandUsage(System.out);
 	}
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
 		// TODO Auto-generated method stub
 		final OperationsParams params = new OperationsParams(new GenericOptionsParser(args));
 		Path[] inputFiles = params.getPaths();
