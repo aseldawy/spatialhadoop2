990a21d391b0cbb42c71f15e9d59b7c0e59c4c1d
Ahmed Eldawy <eldawy@cs.ucr.edu>
2017-07-05 16:54:19 -0700

    Added a missing import

--- src/main/java/edu/umn/cs/spatialHadoop/delaunay/DelaunayTriangulation.java
+++ src/main/java/edu/umn/cs/spatialHadoop/delaunay/DelaunayTriangulation.java
@@ -22,6 +22,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.LocalJobRunner;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
