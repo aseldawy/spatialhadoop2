ca186e101242592ad08ba1327d85b3131ab53e8e
tinvukhac <tinvukhac@gmail.com>
2017-12-07 11:50:02 -0800

    greedy approach to maximize reduced cost

--- src/main/java/edu/umn/cs/spatialHadoop/indexing/IncrementalRTreeIndexer.java
+++ src/main/java/edu/umn/cs/spatialHadoop/indexing/IncrementalRTreeIndexer.java
@@ -7,6 +7,7 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.commons.logging.Log;
@@ -1514,6 +1515,324 @@ public class IncrementalRTreeIndexer {
 				maxCellId = partition.cellId;
 			}
 		}
+		
+		Rectangle inputMBR = (Rectangle) OperationsParams.getShape(conf, "mbr");
+		if(inputMBR == null) {
+			inputMBR = new Rectangle(currentPartitions.get(0));
+			for (Partition p : splittingPartitions) {
+				inputMBR.expand(p);
+			}
+		}
+		double querySize = 0.000001 * Math.sqrt(inputMBR.getSize());
+		System.out.println("Query size = " + querySize);
+		
+		computeReducedCost(currentPartitions, querySize, blockSize);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static void greedyReorganizeWithMaximumReducedCost(Path path, OperationsParams params)
+			throws IOException, InterruptedException, ClassNotFoundException {
+		final byte[] NewLine = new byte[] { '\n' };
+		ArrayList<Partition> currentPartitions = new ArrayList<Partition>();
+		ArrayList<Partition> splittingPartitions = new ArrayList<Partition>();
+		ArrayList<Partition> remainingPartitions = new ArrayList<Partition>();
+		ArrayList<Partition> reorganizedPartitions = new ArrayList<Partition>();
+
+		Job job = new Job(params, "Reorganizer");
+		Configuration conf = job.getConfiguration();
+		FileSystem fs = FileSystem.get(conf);
+		int blockSize = Integer.parseInt(conf.get("dfs.blocksize"));
+		double overflowRate = Double.parseDouble(params.get("overflow_rate"));
+		double overflowSize = blockSize * overflowRate;
+		String sindex = params.get("sindex");
+		double budget = Double.parseDouble(params.get("budget")) * 1024 * 1024;
+
+		// Load current partitions
+		Path currentMasterPath = new Path(path, "_master." + sindex);
+		Text tempLine1 = new Text2();
+		LineReader in1 = new LineReader(fs.open(currentMasterPath));
+		while (in1.readLine(tempLine1) > 0) {
+			Partition tempPartition = new Partition();
+			tempPartition.fromText(tempLine1);
+			currentPartitions.add(tempPartition);
+		}
+		
+		remainingPartitions = (ArrayList<Partition>) currentPartitions.clone();
+
+		int maxCellId = -1;
+		for (Partition partition : currentPartitions) {
+			if (partition.cellId > maxCellId) {
+				maxCellId = partition.cellId;
+			}
+		}
+		
+		Rectangle mbr = (Rectangle) OperationsParams.getShape(conf, "mbr");
+		if(mbr == null) {
+			mbr = new Rectangle(currentPartitions.get(0));
+			for (Partition p : splittingPartitions) {
+				mbr.expand(p);
+			}
+		}
+		double querySize = 0.000001 * Math.sqrt(mbr.getSize());
+		System.out.println("Query size = " + querySize);
+		
+		int budgetBlocks = (int) Math.ceil(budget / blockSize);
+		
+		// Find the partition with maximum reduced cost as a seed for our greedy algorithm
+		Partition maxReducedCostPartition = currentPartitions.get(0);
+		double maxReducedCost = 0;
+		for(Partition p: currentPartitions) {
+			splittingPartitions.add(p);
+			double pReducedCost = computeReducedCost(splittingPartitions, querySize, blockSize);
+			if(maxReducedCost < pReducedCost) {
+				maxReducedCost = pReducedCost;
+				maxReducedCostPartition = p;
+			}
+			splittingPartitions.remove(p);
+		}
+		
+		splittingPartitions.add(maxReducedCostPartition);
+		remainingPartitions.remove(maxReducedCostPartition);
+		budgetBlocks -= maxReducedCostPartition.getNumberOfBlock(blockSize);
+		
+		while(budgetBlocks > 0) {
+			Partition bestCandidatePartition = findBestCandidateToReduceCost(remainingPartitions, splittingPartitions, querySize, blockSize);
+			splittingPartitions.add(bestCandidatePartition);
+			remainingPartitions.remove(bestCandidatePartition);
+			budgetBlocks -= bestCandidatePartition.getNumberOfBlock(blockSize);
+		}
+		
+		// Now splitting partition is the list of partition that promise the biggest reduced cost
+		ArrayList<ArrayList<Partition>> groups = new ArrayList<ArrayList<Partition>>();
+		@SuppressWarnings("unchecked")
+		ArrayList<Partition> tempSplittingPartitions = (ArrayList<Partition>) splittingPartitions.clone();
+		
+		long totalSplitSize = 0;
+		int totalSplitBlocks = 0;
+		while(tempSplittingPartitions.size() > 0) {
+			ArrayList<Partition> group = new ArrayList<Partition>();
+			group.add(tempSplittingPartitions.get(0));
+			for(Partition p: tempSplittingPartitions) {
+				if(isOverlapping(p, group)) {
+					group.add(p);
+				}
+			}
+			groups.add(group);
+			tempSplittingPartitions.removeAll(group);
+		}
+		
+		for(Partition p: splittingPartitions) {
+			totalSplitSize += p.size;
+			totalSplitBlocks += p.getNumberOfBlock(blockSize);
+		}
+		
+		System.out.println("Number of groups = " + groups.size());
+		System.out.println("Total split partitions = " + splittingPartitions.size());
+		System.out.println("Total split size = " + totalSplitSize);
+		System.out.println("Total split blocks = " + totalSplitBlocks);
+
+		// Save split partitions and keep partitions to file
+		/*Path splitPath = new Path(path, "rects.split");
+		Path keepPath = new Path(path, "rects.keep");
+		OutputStream splitOut = fs.create(splitPath);
+		OutputStream keepOut = fs.create(keepPath);
+		for (Partition partition : splittingPartitions) {
+			Text splitLine = new Text2();
+			partition.toText(splitLine);
+			splitOut.write(splitLine.getBytes(), 0, splitLine.getLength());
+			splitOut.write(NewLine);
+		}
+		currentPartitions.removeAll(splittingPartitions);
+		for (Partition partition : currentPartitions) {
+			Text keepLine = new Text2();
+			partition.toText(keepLine);
+			keepOut.write(keepLine.getBytes(), 0, keepLine.getLength());
+			keepOut.write(NewLine);
+		}
+		splitOut.close();
+		keepOut.close();
+
+		// Split selected partitions
+		// Iterate all overflow partitions to split
+		int groupId = 0;
+		ArrayList<RTreePartitioner> partitioners = new ArrayList<RTreePartitioner>();
+		for (ArrayList<Partition> group : groups) {
+			groupId++;
+			Path groupInputPath = new Path("./", "temp.greedy.input" + groupId);
+			Path groupOutputPath = new Path("./", "temp.greedy.output" + groupId);
+
+			// Move all partitions of this group to temporary input
+			if (fs.exists(groupInputPath)) {
+				fs.delete(groupInputPath);
+			}
+			fs.mkdirs(groupInputPath);
+
+			if (fs.exists(groupOutputPath)) {
+				fs.delete(groupOutputPath);
+			}
+
+			Rectangle inputMBR = new Rectangle(group.get(0));
+			for (Partition p : group) {
+				fs.rename(new Path(path, p.filename), new Path(groupInputPath, p.filename));
+				inputMBR.expand(p);
+			}
+
+			OperationsParams params2 = new OperationsParams(conf);
+			params2.setShape(conf, "mbr", inputMBR);
+			params2.setBoolean("local", false);
+
+			// Indexer.index(tempInputPath, tempOutputPath, params2);
+			Job job2 = new Job(params2, "Sampler");
+			Configuration conf2 = job2.getConfiguration();
+			RTreePartitioner groupPartitioner = (RTreePartitioner) Indexer.createPartitioner(groupInputPath,
+					groupOutputPath, conf2, sindex);
+			partitioners.add(groupPartitioner);
+		}
+
+		currentPartitions.removeAll(splittingPartitions);
+		reorganizedPartitions.addAll(currentPartitions);
+
+		GreedyRTreePartitioner greedyPartitioner = new GreedyRTreePartitioner(partitioners, maxCellId);
+		Partitioner.setPartitioner(conf, greedyPartitioner);
+
+		// Set mapper and reducer
+		Path[] tempInputPaths = new Path[groupId];
+		Path tempOutputPath = new Path("./", "temp.greedy.output");
+		for (int i = 0; i < groupId; i++) {
+			Path groupInputPath = new Path("./", "temp.greedy.input" + (i + 1));
+			tempInputPaths[i] = groupInputPath;
+		}
+
+		Shape shape = OperationsParams.getShape(conf, "shape");
+		job.setMapperClass(GreedyRTreePartitionerMap.class);
+		job.setMapOutputKeyClass(IntWritable.class);
+		job.setMapOutputValueClass(shape.getClass());
+		job.setReducerClass(GreedyRTreePartitionerReduce.class);
+		// Set input and output
+		job.setInputFormatClass(SpatialInputFormat3.class);
+		SpatialInputFormat3.setInputPaths(job, tempInputPaths);
+		job.setOutputFormatClass(IndexOutputFormat.class);
+		IndexOutputFormat.setOutputPath(job, tempOutputPath);
+		// Set number of reduce tasks according to cluster status
+		ClusterStatus clusterStatus = new JobClient(new JobConf()).getClusterStatus();
+		job.setNumReduceTasks(Math.max(1,
+				Math.min(greedyPartitioner.getPartitionCount(), (clusterStatus.getMaxReduceTasks() * 9) / 10)));
+
+		// Use multithreading in case the job is running locally
+		conf.setInt(LocalJobRunner.LOCAL_MAX_MAPS, Runtime.getRuntime().availableProcessors());
+
+		// Start the job
+		if (conf.getBoolean("background", false)) {
+			// Run in background
+			job.submit();
+		} else {
+			job.waitForCompletion(conf.getBoolean("verbose", false));
+		}
+
+		// Merge
+		ArrayList<Partition> tempPartitions = new ArrayList<Partition>();
+		Path tempMasterPath = new Path(tempOutputPath, "_master." + sindex);
+		Text tempLine = new Text2();
+		LineReader in = new LineReader(fs.open(tempMasterPath));
+		while (in.readLine(tempLine) > 0) {
+			Partition tempPartition = new Partition();
+			tempPartition.fromText(tempLine);
+			tempPartitions.add(tempPartition);
+		}
+
+		for (Partition p : tempPartitions) {
+			fs.rename(new Path(tempOutputPath, p.filename), new Path(path, p.filename));
+		}
+
+		reorganizedPartitions.addAll(tempPartitions);
+
+		// Update master and wkt file
+		currentMasterPath = new Path(path, "_master." + sindex);
+		Path currentWKTPath = new Path(path, "_" + sindex + ".wkt");
+		fs.delete(currentMasterPath);
+		fs.delete(currentWKTPath);
+		OutputStream masterOut = fs.create(currentMasterPath);
+		PrintStream wktOut = new PrintStream(fs.create(currentWKTPath));
+		wktOut.println("ID\tBoundaries\tRecord Count\tSize\tFile name");
+		for (Partition partition : reorganizedPartitions) {
+			Text masterLine = new Text2();
+			partition.toText(masterLine);
+			masterOut.write(masterLine.getBytes(), 0, masterLine.getLength());
+			masterOut.write(NewLine);
+			wktOut.println(partition.toWKT());
+		}
+
+		wktOut.close();
+		masterOut.close();
+
+		for (int i = 1; i <= groupId; i++) {
+			Path groupInputPath = new Path("./", "temp.greedy.input" + i);
+			fs.delete(groupInputPath);
+		}
+		fs.delete(tempOutputPath);
+
+		for (Partition p : splittingPartitions) {
+			fs.delete(new Path(path, p.filename));
+		}*/
+	}
+	
+	private static Partition findBestCandidateToReduceCost(ArrayList<Partition> currentPartitions, ArrayList<Partition> splittingPartitions, double querySize, int blockSize) {
+		Partition bestPartition = currentPartitions.get(0);
+		double maxReducedCost = 0;
+		for(Partition p: currentPartitions) {
+			splittingPartitions.add(p);
+			double splittingReducedCost = computeReducedCost(splittingPartitions, querySize, blockSize);
+			if(maxReducedCost < splittingReducedCost) {
+				bestPartition = p;
+			}
+			splittingPartitions.remove(p);
+		}
+		
+		return bestPartition;
+	}
+	
+	private static double computeReducedCost(ArrayList<Partition> splittingPartitions, double querySize, int blockSize) {
+//		System.out.println("Computing reduced cost of a set of partitions.");
+		// Group splitting partitions by overlapping clusters
+		ArrayList<ArrayList<Partition>> groups = new ArrayList<ArrayList<Partition>>();
+		@SuppressWarnings("unchecked")
+		ArrayList<Partition> tempSplittingPartitions = (ArrayList<Partition>) splittingPartitions.clone();
+		
+		while(tempSplittingPartitions.size() > 0) {
+			ArrayList<Partition> group = new ArrayList<Partition>();
+			group.add(tempSplittingPartitions.get(0));
+			for(Partition p: tempSplittingPartitions) {
+				if(isOverlapping(p, group)) {
+					group.add(p);
+				}
+			}
+			groups.add(group);
+			tempSplittingPartitions.removeAll(group);
+		}
+		
+//		System.out.println("Number of groups = " + groups.size());
+		
+		// Compute reduced cost
+		double costBefore = 0;
+		for(Partition p: splittingPartitions) {
+			costBefore += (p.getWidth() + querySize) * (p.getHeight() + querySize) * p.getNumberOfBlock(blockSize);
+		}
+		
+		double costAfter = 0;
+		for(ArrayList<Partition> group: groups) {
+			double groupBlocks = 0;
+			Partition tempPartition = group.get(0).clone();
+			for(Partition p: group) {
+				groupBlocks += p.getNumberOfBlock(blockSize);
+				tempPartition.expand(p);
+			}
+			double groupArea = tempPartition.getSize();
+			
+			costAfter += Math.pow((Math.sqrt(groupArea / groupBlocks) + querySize), 2) * groupBlocks;
+		}
+		
+//		System.out.println("Reduced cost = " + Math.abs(costBefore - costAfter));
+		return Math.abs(costBefore - costAfter);
 	}
 	
 	private ArrayList<Partition> getPartitionWithMaximumReducedCost(ArrayList<Partition> partitions, int budget) {
@@ -1531,6 +1850,16 @@ public class IncrementalRTreeIndexer {
 			}
 		}
 
+		return false;
+	}
+	
+	private static boolean isOverlapping(Partition p, ArrayList<Partition> partitions) {
+		for (Partition partition : partitions) {
+			if((p.cellId != partition.cellId) && p.isIntersected(partition)) {
+				return true;
+			}
+		}
+		
 		return false;
 	}
 	
@@ -1567,8 +1896,8 @@ public class IncrementalRTreeIndexer {
 		// The spatial index to use
 		long t1 = System.currentTimeMillis();
 		// Append new data to old data
-		insertMapReduce(currentPath, appendPath, params);
-		appendNewFiles(currentPath, params);
+//		insertMapReduce(currentPath, appendPath, params);
+//		appendNewFiles(currentPath, params);
 		long t2 = System.currentTimeMillis();
 		System.out.println("Total appending time in millis " + (t2 - t1));
 
@@ -1593,8 +1922,11 @@ public class IncrementalRTreeIndexer {
 			System.out.println("Greedy splitting: optimize overlapping");
 			greedyReorganizeWithOverlappingOptimizer(currentPath, params);
 		} else if (splitType.equals("optimal")) {
-			System.out.println("Optiaml splitting: maximize the reduced cost of range queries");
+			System.out.println("Optimal splitting: maximize the reduced cost of range queries");
 			optimalReorganizeWithMaximumReducedCost(currentPath, params);
+		} else if (splitType.equals("greedyreducedcost")) {
+			System.out.println("Greedy splitting: maximize the reduced cost of range queries");
+			greedyReorganizeWithMaximumReducedCost(currentPath, params);
 		}
 
 		long t3 = System.currentTimeMillis();
