e5bb348d1b9864be32a8c51f1df9346d0578209a
Ahmed Eldawy <eldawy@cs.ucr.edu>
2018-01-02 15:55:32 -0800

    Added a new helper class SubPyramid to be used with multilevel plots

---
+++ src/main/java/edu/umn/cs/spatialHadoop/visualization/SubPyramid.java
@@ -0,0 +1,67 @@
+package edu.umn.cs.spatialHadoop.visualization;
+
+import edu.umn.cs.spatialHadoop.core.Rectangle;
+
+/**
+ * Represents a part of the pyramid structure defined by a minimum level,
+ * a maximum level, and a rectangular range of tiles at the maximum level.
+ * Created by Ahmed Eldawy on 1/2/18.
+ */
+public class SubPyramid {
+  /**
+   * The MBR covered by the full pyramid (not this subpyramid).
+   * The range is inclusive for (x1, y1) and exclusive for (x2, y2)
+   */
+  public double x1, y1, x2, y2;
+
+  /** The range of levels (inclusive) covered by the subpyramid */
+  public int minimumLevel, maximumLevel;
+
+  /**
+   * The range of tiles covered by this subpyramid at the maximumLevel
+   * The range includes the first row (r1), the first column (c1), and
+   * excludes the last row (r2) and last column (c2).
+   */
+  public int c1, r1, c2, r2;
+
+  /** The width and height of each tile at maximumLevel */
+  private double tileWidth, tileHeight;
+
+  public SubPyramid(Rectangle mbr, int minLevel, int maxLevel,
+                    int c1, int r1, int c2, int r2) {
+    this.x1 = mbr.x1;
+    this.x2 = mbr.x2;
+    this.y1 = mbr.y1;
+    this.y2 = mbr.y2;
+    this.minimumLevel = minLevel;
+    this.maximumLevel = maxLevel;
+    this.c1 = c1;
+    this.r1 = r1;
+    this.c2 = c2;
+    this.r2 = r2;
+    this.tileWidth = (x2 - x1) / (1 << maximumLevel);
+    this.tileHeight = (y2 - y1) / (1 << maximumLevel);
+  }
+
+  /**
+   * Returns the range of tiles that overlaps the given rectangle at
+   * the maximum level
+   * @param rect
+   * @param overlaps
+   */
+  public void getOverlappingTiles(Rectangle rect, java.awt.Rectangle overlaps) {
+    overlaps.x = (int) Math.floor((rect.x1 - this.x1) / tileWidth);
+    if (overlaps.x < c1)
+      overlaps.x = c1;
+    overlaps.y = (int) Math.floor((rect.y1 - this.y1) / tileHeight);
+    if (overlaps.y < r1)
+      overlaps.y = r1;
+    overlaps.width = (int) Math.ceil((rect.x2 - this.x1) / tileWidth) - overlaps.x;
+    if (overlaps.x + overlaps.width > c2)
+      overlaps.width = c2 - overlaps.x;
+    overlaps.height = (int) Math.ceil((rect.y2 - this.y1) / tileHeight) - overlaps.y;
+    if (overlaps.y + overlaps.height > r2)
+      overlaps.height = r2 - overlaps.y;
+
+  }
+}
---
+++ src/test/java/edu/umn/cs/spatialHadoop/visualization/SubPyramidTest.java
@@ -0,0 +1,73 @@
+package edu.umn.cs.spatialHadoop.visualization;
+
+import edu.umn.cs.spatialHadoop.core.Rectangle;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class SubPyramidTest extends TestCase {
+
+  /**
+   * Create the test case
+   *
+   * @param testName
+   *          name of the test case
+   */
+  public SubPyramidTest(String testName) {
+    super(testName);
+  }
+
+  /**
+   * @return the suite of tests being tested
+   */
+  public static Test suite() {
+    return new TestSuite(SubPyramidTest.class);
+  }
+  
+  public void testOverlappingCells() {
+    Rectangle mbr = new Rectangle(0,0,10,10);
+    SubPyramid subPyramid = new SubPyramid(mbr, 0, 1, 0,0,2,2);
+
+    java.awt.Rectangle overlaps = new java.awt.Rectangle();
+    subPyramid.getOverlappingTiles(new Rectangle(5,5,10,10), overlaps);
+    assertEquals(1, overlaps.x);
+    assertEquals(1, overlaps.y);
+    assertEquals(1, overlaps.width);
+    assertEquals(1, overlaps.height);
+  }
+
+  public void testOverlappingCellsShouldClip() {
+    Rectangle mbr = new Rectangle(0,0,1024,1024);
+    SubPyramid subPyramid = new SubPyramid(mbr, 4, 6, 16,8,20,12);
+    java.awt.Rectangle overlaps = new java.awt.Rectangle();
+
+    // Case 1: No clipping required
+    subPyramid.getOverlappingTiles(new Rectangle(256,128,320,192), overlaps);
+    assertEquals(16, overlaps.x);
+    assertEquals(8, overlaps.y);
+    assertEquals(4, overlaps.width);
+    assertEquals(4, overlaps.height);
+
+    // Case 2: Clip part on the top left
+    subPyramid.getOverlappingTiles(new Rectangle(128,64,320,192), overlaps);
+    assertEquals(16, overlaps.x);
+    assertEquals(8, overlaps.y);
+    assertEquals(4, overlaps.width);
+    assertEquals(4, overlaps.height);
+
+    // Case 3: Clip part on the bottom right
+    subPyramid.getOverlappingTiles(new Rectangle(256,128,512,256), overlaps);
+    assertEquals(16, overlaps.x);
+    assertEquals(8, overlaps.y);
+    assertEquals(4, overlaps.width);
+    assertEquals(4, overlaps.height);
+
+    // Case 4: If completely outside, return an empty rectangle with non-positive width or height
+    subPyramid.getOverlappingTiles(new Rectangle(128,64,196,96), overlaps);
+    assertTrue(overlaps.width <= 0 || overlaps.height <= 0);
+  }
+
+}
